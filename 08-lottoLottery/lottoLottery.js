a642e578feab84326b3c40bababb6c0847e1021c
KoYunHyuk <sksgur3217@gmail.com>
2021-07-31 22:49:15 +0900

    es2021JS

--- 08-lottoLottery/lottoLottery.js
+++ 08-lottoLottery/lottoLottery.js
@@ -7,7 +7,7 @@ let $bonus = document.querySelector('#bonus');
 const candidate = Array(45).fill().map((ele, idx) => idx + 1);
 // 랜덤으로 담을 6자리 숫자
 let shuffle = [];
-// todo: 피셔 예이츠 셔플 (공 뽑기)
+//* 피셔 예이츠 셔플 (공 뽑기)
 while (candidate.length > 0) {
    const random = Math.floor(Math.random() * candidate.length); // 무작위 인덱스 뽑기
    const spliceArray = candidate.splice(random, 1); // 뽑은 값은 배열에 들어있음
@@ -15,7 +15,8 @@ while (candidate.length > 0) {
    shuffle.push(value); // shuffle에 넣어준다
 }
 console.log(shuffle);
-// todo: 공 정렬하기 sort() method
+//* 공 정렬하기 sort() method
+// TODO
 
 /* eventListener */
 /*$result.addEventListener();
