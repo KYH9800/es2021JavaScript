d8a4db35d9998e850dc959bc6c0dae1db36be1a9
고윤혁 <sksgur3217@gmail.com>
2021-08-20 18:54:09 +0900

    ticTacToe-Check To Win Or Lose (2021/08/20)

--- 13-ticTacToe/13-ticTacToe.js
+++ 13-ticTacToe/13-ticTacToe.js
@@ -15,11 +15,72 @@ const $table = document.createElement('table'); // tableTag
 const $result = document.createElement('div'); // resultTag
 
 const rows = []; // 가로열: 이차원 배열 3X3, 배열 안에 배열을 3개 넣어준다
-let turn = 'O'; //* 순서는 'O' 부터 시작
+let turn = 'O'; // 순서는 'O' 부터 시작
+// rows.forEach((row, ri)=>{});
+// [  현재값          인덱스
+// [ td, td, td ], - 0번째 인덱스
+// [ td, td, td ], - 1번쨰 인덱스
+// [ td, td, td ]  - 2번쪠 인덱스
+// ]
+// row.forEach((cell, ci) => {});
+// 0 [ td, td, td ] >> 0, 1, 2
+// 1 [ td, td, td ] >> 0, 1, 2
+// 2 [ td, td, td ] >> 0, 1, 2
+// todo: 승부확인
+const checkWinner = (target) => {
+  let rowIdx;
+  let cellIdx;
+  rows.forEach((row, ri) => { // 현재값, 인덱스
+    row.forEach((cell, ci) => { // 현재값, 인덱스
+      if (cell === target) {
+        rowIdx = ri; // 가로 인덱스
+        cellIdx = ci; // n번째 가로의 칸 인덱스
+        console.log('가로줄: ', ri + '번째 인덱스 줄의 ', '\n세로칸: ', ci + '번째 인덱스 칸');
+      }
+    });
+  });
+  // 세칸은 다 채워졌나?
+  let hasWinner = false; // 검사할 떄는 항상 false로 시작해서 맞으면 true로 바꿔준다
+  // 가로줄 검사
+  if ( //? 가로줄의 인덱스의 모든 값이 일치 하면 승리
+    (rows[rowIdx][0].textContent === turn) &&
+    (rows[rowIdx][1].textContent === turn) &&
+    (rows[rowIdx][2].textContent === turn)
+  ) {
+    hasWinner = true;
+    console.log('가로 빙고 WIN!!');
+  }
+  // 세로줄 검사
+  if ( //? 가로 3줄의 각각의 세로줄 cellIdx 인덱스의 값이 모두 같으면 승리
+    (rows[0][cellIdx].textContent === turn) &&
+    (rows[1][cellIdx].textContent === turn) &&
+    (rows[2][cellIdx].textContent === turn)
+  ) {
+    hasWinner = true;
+    console.log('세로 빙고 WIN!!');
+  }
+  // 대각선 검사
+  if ( //? 대각선의 값들이 일치하면 승리
+    (
+      rows[0][0].textContent === turn &&
+      rows[1][1].textContent === turn &&
+      rows[2][2].textContent === turn
+    ) || // 또는 'or'
+    (
+      rows[0][2].textContent === turn &&
+      rows[1][1].textContent === turn &&
+      rows[2][0].textContent === turn
+    )
+  ) {
+    hasWinner = true;
+    console.log('대각선 빙고 WIN!!');
+  }
+}
 
 //* callback(listenner) function
 const callback = (event) => { //! td 칸 click 시 표시는 여기
-  //? 칸에 글자가 있나? 클자가 있다면, 그 칸은 함수 종료
+  // event.stopPropagation(); // 이벤트 버블링, 캡처링을 막는다
+  // 칸에 글자가 있나? 클자가 있다면, 그 칸은 함수 종료
   // if (event.target.textContent) return; //* 아래와 동일한 동작의 로직
   if (event.target.textContent !== '') {
     console.log('빈칸이 아닙니다');
@@ -27,9 +88,9 @@ const callback = (event) => { //! td 칸 click 시 표시는 여기
   } // else 빈칸이면
   console.log('빈칸입니다')
   event.target.textContent = turn;
-  //* 턴 전환하기
-  turn = (turn === 'O') ? 'X' : 'O'; // 삼항연산자
-  // todo: 승부확인
+  // todo: 승부확인(위의 함수 참고)
+  checkWinner(event.target); // <td> Tag
+  turn = (turn === 'O') ? 'X' : 'O'; // 삼항연산자, 턴 전환하기
 }
 
 // 3X3 테이블 생성 (html 구조 참고)
@@ -49,7 +110,8 @@ for (let i = 0; i < 3; i++) { // tr tag를 table에 3번 넣어주자(반복문 
   rows.push(cells); // 가로행
   console.log('rows: ', rows);
 }
-$table.addEventListener('click', callback); //! 이벤트 버블링
+//! '여기' 설명 참고: 이벤트 버블링, 기본값이 false, true로 하면 캡처링이 된다
+$table.addEventListener('click', callback, false); //! <= '여기'
 $main.append($table); // main tag 안에 테이블을 생성
 $main.append($result); // main tag 안에 결과를 보여줄 태그 생성
 
@@ -108,7 +170,7 @@ if (turn === 'O') {
 }
 */
 /** //* Event Bubbling && Capturing (이벤트 버블링, 캐처링)
- *! 이벤트 버블링이란? (html 현상이므로 반드시 알아두자)
+ *! 이벤트 버블링이란? (html 현상이므로 반드시 알아두자) (하위 >> 상위)
  *  1. html의 특성을 미리 알아보자
  *    - html은 addEventListenner click 이벤트 발생 시 동작이 선택한 태그의 상위 부모 태그들로 올라간다
  *    - ex) td Click >> tr >> table >> body
@@ -117,4 +179,7 @@ if (turn === 'O') {
  *  4. 만일 callback 함수 안에서 table을 가져오고 싶다면, event.currentTarget 하면된다
  *  5. event.target은 target이 누가 될지 모르는 반면, event.currentTarget은 항상 확실하게 지정한 태그만 가져온다
  *  6. 제한을 두기보다는 잘 이해하고 잘 쓰면된다
+ * 
+ * ! 이벤트 캡처링은 버블링과 반대다 (상위 >> 하위)
+ * - 팝업의 밖같 쪽을 클릭 시 팝업창이 닫히도록 하는데 많이 쓰이는 기술이다
  */
