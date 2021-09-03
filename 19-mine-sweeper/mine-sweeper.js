81bb6afa9981b4112912348db35b8bac643eeb05
고윤혁 <sksgur3217@gmail.com>
2021-09-03 19:25:33 +0900

    start mine-sweeper(es2021JS/09/03/Fri)

---
+++ 19-mine-sweeper/mine-sweeper.js
@@ -0,0 +1,6 @@
+// todo: 12장 재귀함수 (지뢰찾기)
+// todo: 13장 키보드 마우스 이벤트
+// todo: 14장 복습
+const $tbody = document.querySelector('#table tbody');
+const $result = document.querySelector('#result');
+// 재귀함수를 통해 지뢰찾기를 구현하세요
